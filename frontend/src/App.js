2d1a21b72083fa61ca250f82d95d0d9f64c13530
arcasyardbakeshop <arcasyardbakeshop@gmail.com>
2026-03-12 14:08:00 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 
 import { CartProvider } from "./context/CartContext";
-import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
@@ -22,15 +21,10 @@ function App() {
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
-            <Route
-              path="/order-confirmation/:orderId"
-              element={<OrderConfirmation />}
-            />
+            <Route path="/order-confirmation/:orderId" element={<OrderConfirmation />} />
             <Route path="/admin" element={<AdminLogin />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
           </Routes>
-
-          <Toaster position="top-center" richColors />
         </div>
       </BrowserRouter>
     </CartProvider>
