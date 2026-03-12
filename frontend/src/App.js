2e2f855b4fedd9a1e51860662132f0a287e15bbd
arcasyardbakeshop <arcasyardbakeshop@gmail.com>
2026-03-12 13:58:46 +0800

    Fix import quotes and format OrderConfirmation route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '@/App.css';
-import Home from './pages/Home';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import OrderConfirmation from './pages/OrderConfirmation';
-import AdminLogin from './pages/AdminLogin';
-import AdminDashboard from './pages/AdminDashboard';
-import { CartProvider } from './context/CartContext';
-import { Toaster } from '@/components/ui/sonner';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import OrderConfirmation from "./pages/OrderConfirmation";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+
+import { CartProvider } from "./context/CartContext";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
@@ -20,10 +22,14 @@ function App() {
             <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
-            <Route path="/order-confirmation/:orderId" element={<OrderConfirmation />} />
+            <Route
+              path="/order-confirmation/:orderId"
+              element={<OrderConfirmation />}
+            />
             <Route path="/admin" element={<AdminLogin />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
           </Routes>
+
           <Toaster position="top-center" richColors />
         </div>
       </BrowserRouter>
